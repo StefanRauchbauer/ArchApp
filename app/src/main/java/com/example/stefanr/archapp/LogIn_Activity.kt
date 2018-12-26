389a4012e42283cae40f604431d8295025c13075
StefanR <abc>
2018-12-26 16:22:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/stefanr/archapp/LogIn_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.stefanr.archapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LogIn_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in_)
+    }
+}
