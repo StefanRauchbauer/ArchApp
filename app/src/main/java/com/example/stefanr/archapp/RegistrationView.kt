fa92f3a371d1692a98562bbd71468a5117fc64e6
StefanR <abc>
2018-12-27 14:15:55 +0100

    Added RegistrationView

--- app/src/main/java/com/example/stefanr/archapp/RegistrationView.kt
+++ app/src/main/java/com/example/stefanr/archapp/RegistrationView.kt
@@ -5,19 +5,14 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_log_in_.*
 
-class LogIn_Activity : AppCompatActivity() {
+class RegistrationView : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in_)
+        setContentView(R.layout.activity_registration_view)
 
-        txt_register.setOnClickListener{
-            register()
-        }
+
     }
 
-    private fun register()
-    {
-        startActivity(Intent(this,RegistrationView::class.java))
-    }
+
 }
