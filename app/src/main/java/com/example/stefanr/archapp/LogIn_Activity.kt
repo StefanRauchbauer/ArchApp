60ae1c039269a33e8434acb310dfb22af726070a
StefanR <abc>
2018-12-28 16:05:15 +0100

    Registration and Login with firebase finally working

--- app/src/main/java/com/example/stefanr/archapp/LogIn_Activity.kt
+++ app/src/main/java/com/example/stefanr/archapp/LogIn_Activity.kt
@@ -3,6 +3,9 @@ package com.example.stefanr.archapp
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_log_in_.*
 
 class LogIn_Activity : AppCompatActivity() {
@@ -10,6 +13,10 @@ class LogIn_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in_)
+
+        btn_login.setOnClickListener{
+            login()
+        }
 
         txt_register.setOnClickListener{
             register()
@@ -19,5 +26,24 @@ class LogIn_Activity : AppCompatActivity() {
     private fun register()
     {
         startActivity(Intent(this,RegistrationView::class.java))
+    }
+
+    private fun login()
+    {
+        val firebase= FirebaseAuth.getInstance()
+
+        var email = input_email.text.toString()
+        var password = input_password.text.toString()
+
+        if(email.isNotEmpty() && password.isNotEmpty())
+        {
+            firebase.signInWithEmailAndPassword(email,password).addOnCompleteListener {
+                if(it.isSuccessful)
+                {
+                    Log.e("nain", "Successfull Login")
+                    startActivity(Intent(this, MenueView::class.java))
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/stefanr/archapp/RegistrationView.kt
+++ app/src/main/java/com/example/stefanr/archapp/RegistrationView.kt
@@ -3,7 +3,10 @@ package com.example.stefanr.archapp
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_log_in_.*
+import kotlinx.android.synthetic.main.activity_registration_view.*
 
 class RegistrationView : AppCompatActivity() {
 
@@ -11,6 +14,33 @@ class RegistrationView : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration_view)
 
+
+        btn_register.setOnClickListener{
+            register()
+        }
+
+
+    }
+
+    private fun register(){
+
+        val email = input_email_register.text.toString()
+        val password = input_password_register.text.toString()
+        startActivity( Intent(this,LogIn_Activity::class.java))
+
+        val firebase= FirebaseAuth.getInstance()
+        if(firebase != null) {
+            Log.e("nain", "Not Null"+ firebase.toString())
+            firebase.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (!it.isSuccessful) {
+                    Log.e("nain", "Not Successfull" + "Email " + email + "Passwort " + password)
+                    return@addOnCompleteListener
+                } else {
+                    Log.e("nain", "Successfull")
+
+                }
+            }
+        }
 
     }
 
